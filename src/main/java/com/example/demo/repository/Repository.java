fd67de3117c0d1cf498ee06e18fdca8ecda07acb
jgarnaiz <86413489+jgarnaiz@users.noreply.github.com>
2021-06-24 14:26:48 +0200

    Delete src/main directory

--- src/main/java/com/example/demo/repository/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.repository.model.TweetDb;
-
-public interface Repository extends JpaRepository<TweetDb, Integer> {
-
-}
